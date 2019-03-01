6c54701f7d238edc62b4bb92be9eaccae4011c7a
suppergerrie2 <gerjan2001@gmail.com>
2019-03-01 20:03:17 +0100

    Cleaned up CCOrgDetailView and made buttons stay at the right positions when the window scales

--- src/main/java/com/schematical/chaoscraft/gui/CCGuiButton.java
+++ src/main/java/com/schematical/chaoscraft/gui/CCGuiButton.java
@@ -8,15 +8,25 @@ import net.minecraft.entity.Entity;
  * Created by user1a on 2/21/19.
  */
 public class CCGuiButton extends GuiButton {
-    protected  EntityOrganism entity;
+    protected EntityOrganism entity;
     public String action;
+
+    @Deprecated
     public CCGuiButton(int buttonId, int x, int y, int width, int height, String buttonText) {
         super(buttonId, x, y, width, height, buttonText);
     }
-    public void setEntity(EntityOrganism _entity){
-        this.entity = _entity;
+
+    public CCGuiButton(int buttonId, int x, int y, int width, int height, String buttonText, String action) {
+        this(buttonId, x, y, width, height, buttonText);
+        this.action = action;
     }
-    public EntityOrganism getEntity(){
+
+    public CCGuiButton(int buttonId, int x, int y, int width, int height, String buttonText, String action, EntityOrganism entity) {
+        this(buttonId, x, y, width, height, buttonText, action);
+        this.entity = entity;
+    }
+
+    public EntityOrganism getEntity() {
         return this.entity;
     }
 }
