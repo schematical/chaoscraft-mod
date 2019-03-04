12ab5fff9804d45aa91b60761e245765c73c4c03
Matt <mlea@schematical.com>
2019-03-04 12:12:40 -0600

    Added a BiasInput

---
+++ src/main/java/com/schematical/chaoscraft/ai/inputs/BiasInput.java
@@ -0,0 +1,36 @@
+package com.schematical.chaoscraft.ai.inputs;
+
+import com.schematical.chaoscraft.ai.CCAttributeId;
+import com.schematical.chaoscraft.ai.CCObserviableAttributeCollection;
+import com.schematical.chaoscraft.ai.InputNeuron;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import org.json.simple.JSONObject;
+
+/**
+ * Created by user1a on 12/8/18.
+ */
+public class BiasInput extends InputNeuron {
+    //TODO: Add a specific slot
+    public float weight;
+
+
+    @Override
+    public float evaluate(){
+        this._lastValue = weight;
+        return this._lastValue;
+    }
+    @Override
+    public void parseData(JSONObject jsonObject) {
+        super.parseData(jsonObject);
+        weight =  Float.parseFloat(jsonObject.get("weight").toString());
+
+    }
+    public String toLongString(){
+        String response = super.toLongString();
+        response += " " + this.weight;
+        return response;
+
+    }
+
+}
